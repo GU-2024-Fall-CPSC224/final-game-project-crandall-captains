95f5feb98e9f2e3d7ce63082631743d9d04c174a
SAMV <samwv@icloud.com>
2024-11-25 12:11:06 -0800

    Beginning of Piece Class

--- src/main/java/edu/gonzaga/Piece.java
+++ src/main/java/edu/gonzaga/Piece.java
@@ -5,6 +5,28 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class Piece {
-    
+public abstract class Piece {
+    private String color;
+    private int row;
+    private int col; 
+
+    public Piece(String color, int row, int col) {
+        this.color = color;
+        this.row = row;
+        this.col = col;
+    }
+    public String getColor() {
+        return color;
+    }
+    public int getRow() {
+        return row;
+    }
+    public int getCol() {
+        return col; 
+    }
+    public void setPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+    public abstract boolean isValidMove(int destRow, int destCol, Board board);
 }
