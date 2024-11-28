b122eae918560d24329eb1a0384b7ca79ad8c592
SAMV <samwv@icloud.com>
2024-11-28 00:12:28 -0800

    added has moved method to piece and castling method in king

--- src/main/java/edu/gonzaga/King.java
+++ src/main/java/edu/gonzaga/King.java
@@ -5,6 +5,37 @@ public class King extends Piece {
     public King(String color, int row, int col) {
         super(color, row, col);
     }
+    private boolean isCastlingMove(int destRow, int destCol, Board board) {
+        if (destRow != getRow()) {
+            return false;
+        }
+        int colDiff = destCol - getCol();
+        if (Math.abs(colDiff) != 2) {
+            return false;
+        }
+        if(this.getHasMoved() == true) {
+            return false;
+        }
+        int rookCol = (colDiff > 0) ? 7 : 0;
+        Piece rook = board.getPiece(getRow(), rookCol);
+        if(rook == null || !(rook instanceof Rook) || (rook.getHasMoved() == true)) {
+            return false;
+        }
+        int direction = colDiff > 0 ? 1  : -1; 
+        for (int col = getCol() + direction; col != rookCol; col +=direction ) {
+            if(board.getPiece(getRow(), col) != null) {
+                return false;
+            }
+        }
+         /* 
+        for(int col = getCol(); col != destCol + direction; col += direction) {
+            if(board.isSquareUnderAttack(getRow(), col, getColor()) == true){
+                return false;
+            }
+        }
+        */
+        return true;
+    }
 
     @Override
     public boolean isValidMove(int destRow, int destCol, Board board) {
@@ -12,9 +43,14 @@ public class King extends Piece {
         int colDiff = Math.abs(destCol - getCol());
 
         // King moves one square in any direction
-        return (rowDiff <= 1 && colDiff <= 1) &&
-               (board.getPiece(destRow, destCol) == null ||
+        if (rowDiff <= 1 && colDiff <= 1){
+               return (board.getPiece(destRow, destCol) == null ||
                 !board.getPiece(destRow, destCol).getColor().equalsIgnoreCase(getColor()));
+        }
+        if (isCastlingMove(destRow, destCol, board)) {
+            return true;
+        }
+        return false;
     }
 
     @Override
--- src/main/java/edu/gonzaga/Piece.java
+++ src/main/java/edu/gonzaga/Piece.java
@@ -6,6 +6,7 @@ public abstract class Piece {
     private String color; // Piece color (White or Black)
     private int row;      // Row position
     private int col;      // Column position
+    private boolean hasMoved;
 
     public Piece(String color, int row, int col) {
         this.color = color;
@@ -24,12 +25,17 @@ public abstract class Piece {
     public int getCol() {
         return col;
     }
+    public boolean getHasMoved() {
+        return hasMoved;
+    }
 
     public void setPosition(int row, int col) {
         this.row = row;
         this.col = col;
+        this.hasMoved = true;
     }
 
+    
     // Abstract method for validating moves (to be implemented by subclasses)
     public abstract boolean isValidMove(int destRow, int destCol, Board board);
 
